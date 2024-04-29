ed4874e9428a5a795beca3ec2e2a41caa424fbd9
unknown <wangz282@uw.edu>
2024-04-28 23:26:54 -0700

    handled input and output properly

--- Basic.java
+++ Basic.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,20 +15,51 @@ public class Basic {
     };
 
     // Other methods
+    private static String dp(String string1, String string2) {
+        // TODO: implement basic dp
+        String result = string1 + " " + string2;
+
+        return result;
+    }
 
     // Main Method
     public static void main(String[] args) throws Exception {
+        String inputFilePath = "datapoints/in2.txt";
+        String outputFilePath = "output.txt";
+
+        if (args.length > 0) {
+            inputFilePath = args[0];
+        } 
+        if (args.length > 1) {
+            outputFilePath = args[1];
+        }
+
         try {
-            StringGenerator generator = new Basic().new StringGenerator("datapoints/in1.txt");
+            // generate standard input
+            StringGenerator generator = new Basic().new StringGenerator(inputFilePath);
             String string1 = generator.getStrings()[0];
             String string2 = generator.getStrings()[1];
 
-            System.out.println(string1 + " " + string2);
+            // calculate
+            String output = dp(string1, string2);
+            
+            // output result to a file
+            File file = new File(outputFilePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
 
+            FileWriter writer = new FileWriter(file, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(output);
+            
+            bufferedWriter.close();
         } catch (IOException e) {
             System.err.println("Error reading input file: " + e.getMessage());
         }
     }
+
+    
 
     // Helper Classes
     public class StringGenerator {
